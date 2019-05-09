5b10c8819ee69b20a4dd16bde9b154dede6e29d6
syq <shenyongqiang2008@126.com>
2019-05-09 10:36:41 +0800

    初次添加压缩和解压缩源代码

---
+++ ZipCompressTest/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ZipCompressTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnZipFlo = new System.Windows.Forms.Button();
+            this.btnUnZipFlo = new System.Windows.Forms.Button();
+            this.btnBatchZipFlo = new System.Windows.Forms.Button();
+            this.btnBatchUnZipFlo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnZipFlo
+            // 
+            this.btnZipFlo.Location = new System.Drawing.Point(47, 37);
+            this.btnZipFlo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnZipFlo.Name = "btnZipFlo";
+            this.btnZipFlo.Size = new System.Drawing.Size(70, 31);
+            this.btnZipFlo.TabIndex = 0;
+            this.btnZipFlo.Text = "压缩文件";
+            this.btnZipFlo.UseVisualStyleBackColor = true;
+            this.btnZipFlo.Click += new System.EventHandler(this.btnZipFlo_Click);
+            // 
+            // btnUnZipFlo
+            // 
+            this.btnUnZipFlo.Location = new System.Drawing.Point(47, 119);
+            this.btnUnZipFlo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnUnZipFlo.Name = "btnUnZipFlo";
+            this.btnUnZipFlo.Size = new System.Drawing.Size(70, 31);
+            this.btnUnZipFlo.TabIndex = 1;
+            this.btnUnZipFlo.Text = "解压文件";
+            this.btnUnZipFlo.UseVisualStyleBackColor = true;
+            this.btnUnZipFlo.Click += new System.EventHandler(this.btnUnZipFlo_Click);
+            // 
+            // btnBatchZipFlo
+            // 
+            this.btnBatchZipFlo.Location = new System.Drawing.Point(190, 37);
+            this.btnBatchZipFlo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBatchZipFlo.Name = "btnBatchZipFlo";
+            this.btnBatchZipFlo.Size = new System.Drawing.Size(93, 31);
+            this.btnBatchZipFlo.TabIndex = 2;
+            this.btnBatchZipFlo.Text = "批量压缩文件";
+            this.btnBatchZipFlo.UseVisualStyleBackColor = true;
+            this.btnBatchZipFlo.Click += new System.EventHandler(this.btnBatchZipFlo_Click);
+            // 
+            // btnBatchUnZipFlo
+            // 
+            this.btnBatchUnZipFlo.Location = new System.Drawing.Point(190, 119);
+            this.btnBatchUnZipFlo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnBatchUnZipFlo.Name = "btnBatchUnZipFlo";
+            this.btnBatchUnZipFlo.Size = new System.Drawing.Size(93, 31);
+            this.btnBatchUnZipFlo.TabIndex = 3;
+            this.btnBatchUnZipFlo.Text = "批量解压文件";
+            this.btnBatchUnZipFlo.UseVisualStyleBackColor = true;
+            this.btnBatchUnZipFlo.Click += new System.EventHandler(this.btnBatchUnZipFlo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 190);
+            this.Controls.Add(this.btnBatchUnZipFlo);
+            this.Controls.Add(this.btnBatchZipFlo);
+            this.Controls.Add(this.btnUnZipFlo);
+            this.Controls.Add(this.btnZipFlo);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnZipFlo;
+        private System.Windows.Forms.Button btnUnZipFlo;
+        private System.Windows.Forms.Button btnBatchZipFlo;
+        private System.Windows.Forms.Button btnBatchUnZipFlo;
+    }
+}
+
---
+++ ZipCompressTest/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZipCompressTest.Model;
+
+namespace ZipCompressTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 压缩事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnZipFlo_Click(object sender, EventArgs e)
+        {
+            string[] strs = new string[2];
+            //待压缩文件目录
+            strs[0] = "D:\\DeBug1\\";
+            //压缩后的目标文件
+            strs[1] = "D:\\Debug2\\FrpTest.zip";
+            ZipFloClass zc = new ZipFloClass();
+            zc.ZipFile(strs[0], strs[1]);
+        }
+
+        /// <summary>
+        /// 解压事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnUnZipFlo_Click(object sender, EventArgs e)
+        {
+            string[] strs = new string[2];
+            string msg = "";
+            //待解压的文件
+            strs[0] = "D:\\Debug2\\FrpTest.zip";
+            //解压后放置的目标文件
+            strs[1] = "D:\\Debug3";
+            UnZipFloClass uzc = new UnZipFloClass();
+            uzc.unZipFile(strs[0], strs[1], ref msg);
+            MessageBox.Show("信息：" + msg);
+        }
+
+        /// <summary>
+        /// 批量压缩事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBatchZipFlo_Click(object sender, EventArgs e)
+        {
+            string path1 = "D:\\DeBug1\\";   //待压缩的目录文件
+            string path2 = "D:\\Debug2\\";   //压缩后存放目录文件
+            //获取指定目录下所有文件和子文件名称(所有待压缩的文件)
+            string[] files = Directory.GetFileSystemEntries(path1);
+            ZipFloClass zc = new ZipFloClass();
+            //遍历指定目录下文件路径
+            foreach (string file in files)
+            {
+                //截取文件路径的文件名
+                var filename = file.Substring(file.LastIndexOf("\\") + 1);
+                //调用压缩方法（参数1：待压缩的文件目录，参数2：压缩后的文件目录（包含后缀））
+                zc.ZipFile(path1 + filename, path2 + filename + ".zip");
+            }
+        }
+
+        /// <summary>
+        /// 批量解压事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnBatchUnZipFlo_Click(object sender, EventArgs e)
+        {
+            string msg = "";
+            string path2 = "D:\\Debug2\\";
+            string path3 = "D:\\Debug3\\";
+            //获取指定目录下所有文件和子文件名称（所有待解压的压缩文件）
+            string[] files = Directory.GetFileSystemEntries(path2);
+            UnZipFloClass uzc = new UnZipFloClass();
+            //遍历所有压缩文件路径
+            foreach (string file in files)
+            {
+                //获取压缩包名称（包括后缀名）
+                var filename  = file.Substring(file.LastIndexOf("\\") + 1);
+                //得到压缩包名称（没有后缀）
+                filename = filename.Substring(0, filename.LastIndexOf("."));
+                //判断解压的路径是否存在
+                if (!Directory.Exists(path3 + filename))
+                {
+                    //没有，则创建这个路径
+                    Directory.CreateDirectory(path3 + filename);
+                }
+                //调用解压方法（参数1：待解压的压缩文件路径（带后缀名），参数2：解压后存放的文件路径，参数3：返工是否解压成功）
+                uzc.unZipFile(file, path3 + filename, ref msg);
+            }
+            MessageBox.Show("批量解压成功");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
